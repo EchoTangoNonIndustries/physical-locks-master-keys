e81b7fe68df6672926162e348708bf16b74d7d06
EliaTarasoff <elia.tarasoff@gmail.com>
2023-08-25 11:11:56 -0600

    wip: mastering code

--- mastering/mastering.go
+++ mastering/mastering.go
@@ -4,14 +4,34 @@ import "errors"
 
 const (
 	MINIMUM_MASTER_PIN_SIZE = 2 // to avoid pins jamming sideways
+	LEAST_MASTERED_PINS     = 2
+	MOST_MASTERED_PINS      = 5
 )
 
-// LockPinSet counts pin stacks from the shoulder of the key to the tip.
-type LockPinSet []PinStack
+func GetKeysAndLocks(rooms []Room, keysToKeep []Key) (keys []Key, locks []Lock, err error) {
+	return nil, nil, errors.New("UNIMPLEMENTED")
+}
+
+type Room struct {
+	Name     string
+	Owner    string
+	OpenedBy []string
+}
+
+type Key struct {
+	Owner string
+	Cuts  []int
+}
+
+// Lock counts pin stacks from outside to the inside.
+type Lock struct {
+	Room string
+	Pins []PinStack
+}
+
+func (lock *Lock) OpensWith(k *Key) bool {
+	return false
+}
 
 // PinStack is a list of pin sizes, starting from the lowest (key) pin.
 type PinStack []int
-
-func foo() (*LockPinSet, error) {
-	return nil, errors.New("UNIMPLEMENTED")
-}
--- schlage/schlage.go
+++ schlage/schlage.go
@@ -4,4 +4,6 @@ const (
 	MAXIMUM_ADJACENT_CUT_SPECIFICATION = 7
 	MAXIMUM_CUT_DEPTH                  = 9
 	MINIMUM_CUT_DEPTH                  = 0
+	LEAST_PINS                         = 5
+	MOST_PINS                          = 6
 )
